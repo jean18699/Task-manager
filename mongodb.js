0761ea3f36c8381c13ad17095c72259fcb8e7dc2
Jean Carlos George <jean18699@gmail.com>
2022-10-03 00:26:19 -0400

    first commit

---
+++ mongodb.js
@@ -0,0 +1,20 @@
+const mongodb = require('mongodb')
+const client = mongodb.MongoClient;
+
+const connectionUrl = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+
+client.connect(connectionUrl, {useNewUrlParser: true}, (error, client)=>{
+    if(error){
+        return console.log("Unable to connect to database!");
+    }
+    
+    const db = client.db(databaseName)
+    
+    const users = db.collection('users').insertOne({
+        name: 'Andrew',
+        age: 27
+    })
+
+})
