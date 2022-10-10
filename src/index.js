eb44ef35759dbf0505321fbf123be86780dc63cd
Jean Carlos George <jean18699@gmail.com>
2022-10-10 17:19:29 -0400

    ading password encrypt

--- src/index.js
+++ src/index.js
@@ -15,5 +15,23 @@ app.listen(port, ()=>{
     console.log('connected to port ' + port)
 });
 
+const bcrypt = require('bcryptjs');
 
+const myFunction = async()=>{
+    const password = 'Red12345';
+    const hashedPassword = await bcrypt.hash(password, 8);
+    
+
+    console.log(password);
+    
+    console.log(hashedPassword);
+    
+    
+    const isMatch = await bcrypt.compare(password, hashedPassword2);
+
+    console.log(isMatch);
+}
+
+
+myFunction()
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,40 +1,49 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid')
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            validate(value){
+                if(!validator.isEmail(value)){
+                    throw new Error('Email is invalid')
+                }
+            }
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+            trim: true,
+            validate(value){
+                if(value.includes('password')){
+                    throw new Error('Password cant contain the word password')
+                }
             }
         }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-        trim: true,
-        validate(value){
-            if(value.includes('password')){
-                throw new Error('Password cant contain the word password')
+        ,
+        age: {
+            type: Number,
+            validate(value){
+                if(value < 0){
+                    throw new Error('Age must be a positive number')
+                }
             }
         }
     }
-    ,
-    age: {
-        type: Number,
-        validate(value){
-            if(value < 0){
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
+)
+
+userSchema.pre('save', async function(next){
+    const user = this;
+    
 });
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
