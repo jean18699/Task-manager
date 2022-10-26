d9a231a30a3155ae23fda54d30b20f998e11ff62
Jean Carlos George <jean18699@gmail.com>
2022-10-26 06:44:25 -0400

    cors updated

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 
 require('./db/mongoose');
 
 const userRouter = require('./routers/userRouter');
 const taskRouter = require('./routers/taskRouter');
 
+app.use(cors({
+    origin: 'http://localhost:4200'
+}))
 app.use(express.json()); //esto debe ir antes de las llamadas a los routers o no se podran leer los request.body
 app.use(userRouter, taskRouter);
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const app = require('./app');r);
+const app = require('./app');
+
+const port = process.env.PORT
 
 app.listen(port, ()=>{
     console.log('connected to port ' + port)
